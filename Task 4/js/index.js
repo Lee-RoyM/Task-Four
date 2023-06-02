049fc7077c53c5e9e2eb8432d0fcdef024e2154e
Lee-RoyM <125883288+Lee-RoyM@users.noreply.github.com>
2023-06-02 14:56:13 +0200

    Add files via upload

---
+++ Task 4/js/index.js
@@ -0,0 +1,19 @@
+
+addCar = () => {
+    let type = document.getElementById("type").value
+    let make = document.getElementById("make").value
+    let model = +document.getElementById("model").value
+    let condition = document.getElementById("condition").value
+
+    if(age >= 18){
+        alert("vehicle: " + first + " " + last + ", has been added to " + subject);
+    } else {
+        alert("vehicle Inventory Log");
+    }
+
+}
+
+
+
+
+
